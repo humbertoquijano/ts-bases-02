97eda2131c6671ae106a3aab100ad8fba57ecf36
Humberto Quijano <humberto.quijano@gmail.com>
2023-12-03 17:52:31 -0500

    implementacin de primer componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'First App Angular';
-  public counter: number = 10;
-
-  increaseBy(value: number):void {
-    this.counter += value;
-  }
-
-  decreaseBy(value: number):void {
-    this.counter -= value;
-  }
-
-  resetCounter(): void {
-    this.counter= 10;
-  }
 }
